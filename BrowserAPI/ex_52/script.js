16a6d3261a6791408e9b4e5fdb5587a35a28b8dd
rebecatschiedel <rebecatcmg@gmail.com>
2021-03-02 15:27:41 -0800

    exercise 52

--- BrowserAPI/ex_52/script.js
+++ BrowserAPI/ex_52/script.js
@@ -1,11 +1,38 @@
 let url = 'https://swapi.dev/api/films/';
 
-fetch(url).then(function(response){
-    return response.json();   
+let div = document.createElement('div');
+
+fetch(url).then(function(response){   
+    return response.json();
 })
 .then(function(data) {
-    console.log(data);
+
+    data.results.forEach(film => {
+        const title = film.title;
+        const episode_id = film.episode_id;
+        const release_date = film.release_date;
+        
+        const h1Title = document.createElement('h1');
+        const pEpisode = document.createElement('p');
+        const pRelease = document.createElement('p');
+
+        h1Title.innerText = `title: ${title}`;
+        pEpisode.innerText = `episode_id: ${episode_id}`;
+        pRelease.innerText = `release_date: ${release_date}`;
+
+        div.appendChild(h1Title);
+        div.appendChild(pEpisode);
+        div.appendChild(pRelease);
+
+    });
+
+
+    const film = data.results[0];
+    console.log(film);
 })
+.catch(function(err) {
+    console.log('Fetch problem: ' + err.message);
+}); 
 
-
+document.body.appendChild(div);
 
